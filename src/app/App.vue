13c058d7fe69f5e06e96a545272983f6081e00a6
marvin <marvin@marvinisaac.com>
2020-06-23 09:57:52 +0800

    :construction: Set up project router

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,20 +1,10 @@
 <template>
     <div class="hero is-fullheight">
         <div class="hero-body">
-            <home></home>
+            <router-view></router-view>
         </div>
     </div>
 </template>
-
-<script>
-import Home from './pages/Home.vue'
-
-export default {
-    components: {
-        Home
-    }
-}
-</script>
 
 <style lang="scss" scoped>
 .hero {
---
+++ src/app/components/Router.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from './../pages/Home.vue'
+import Blog from './../pages/Blog.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+    {
+        path: '/',
+        component: Home
+    }, {
+        path: '*',
+        redirect: '/'
+    }
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    base: '/',
+    routes
+})
+
+export default router
